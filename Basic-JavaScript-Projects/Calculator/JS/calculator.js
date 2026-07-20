c06f3831cb4a12b5150b35f2927ebd4002b3d0de
jusehiemua <jusehiemua@yahoo.com>
2026-07-20 03:18:37 -0400

    feat: add JavaScript calculator project

---
+++ Basic-JavaScript-Projects/Calculator/JS/calculator.js
@@ -0,0 +1,90 @@
+// JavaScript Calculator Logic
+const Calculator = {
+    Display_Value: '0',
+    First_Operand: null,
+    Wait_Second_Operand: false,
+    operator: null,
+};
+
+// Modifies values when a button is clicked
+function Input_Digit(digit) {
+    const { Display_Value, Wait_Second_Operand } = Calculator;
+    if (Wait_Second_Operand === true) {
+        Calculator.Display_Value = digit;
+        Calculator.Wait_Second_Operand = false;
+    } else {
+        Calculator.Display_Value = Display_Value === '0' ? digit : Display_Value + digit;
+    }
+}
+
+// Handles decimal points
+function Input_Decimal(dot) {
+    if (Calculator.Wait_Second_Operand === true) return;
+    if (!Calculator.Display_Value.includes(dot)) {
+        Calculator.Display_Value += dot;
+    }
+}
+
+// Handles operators and calculations
+function Handle_Operator(Next_Operator) {
+    const { First_Operand, Display_Value, operator } = Calculator;
+    const Value_of_Input = parseFloat(Display_Value);
+
+    if (operator && Calculator.Wait_Second_Operand) {
+        Calculator.operator = Next_Operator;
+        return;
+    }
+
+    if (First_Operand == null) {
+        Calculator.First_Operand = Value_of_Input;
+    } else if (operator) {
+        let result = Perform_Calculation[operator](First_Operand || 0, Value_of_Input);
+        result = Number(result).toFixed(9);
+        result = (result * 1).toString();
+        Calculator.Display_Value = parseFloat(result);
+        Calculator.First_Operand = parseFloat(result);
+    }
+
+    Calculator.Wait_Second_Operand = true;
+    Calculator.operator = Next_Operator;
+}
+
+const Perform_Calculation = {
+    '/': (f, s) => f / s,
+    '*': (f, s) => f * s,
+    '+': (f, s) => f + s,
+    '-': (f, s) => f - s,
+    '=': (f, s) => s
+};
+
+function Calculator_Reset() {
+    Calculator.Display_Value = '0';
+    Calculator.First_Operand = null;
+    Calculator.Wait_Second_Operand = false;
+    Calculator.operator = null;
+}
+
+function Update_Display() {
+    const display = document.querySelector('.calculator-screen');
+    display.value = Calculator.Display_Value;
+}
+
+Update_Display();
+
+// Monitors button clicks
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', (event) => {
+    const { target } = event;
+    if (!target.matches('button')) return;
+
+    if (target.classList.contains('operator')) {
+        Handle_Operator(target.value);
+    } else if (target.classList.contains('decimal')) {
+        Input_Decimal(target.value);
+    } else if (target.classList.contains('all-clear')) {
+        Calculator_Reset();
+    } else {
+        Input_Digit(target.value);
+    }
+    Update_Display();
+});
